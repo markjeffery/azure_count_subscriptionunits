2635fd50f8c032476c22464df89e5938e58c7b8d
Mark Jeffery <mark@jeffery.com>
2024-12-18 12:32:20 +0000

    Update azure_resources.ps1
    Changed rounding to round up using Math Ceiling function

--- azure_resources.ps1
+++ azure_resources.ps1
@@ -1,10 +1,10 @@
 $total_count = 0
 $sub_count = 0
 $su_count = 0
-
+ 
 # Get Azure Subscriptions ID's
 $GroupList = az account list --query '[].id'
-
+ 
 # Loop for each Azure Subscription ID
 foreach ($id in $GroupList) {
     if ($id.length -gt 4) {
@@ -22,10 +22,11 @@ foreach ($id in $GroupList) {
             $sub_count = $sub_count + $vm
         }
         echo "Subtotal for Virtual Machines $sub_count"
-
+ 
         $sub_count = 0
         $su_count = 0
         foreach ($line in Get-Content .\resources.azure.list) {
+          
             # echo "Line_ID $line for Id_Comp $Id_Comp"
             if ($line.equals("Microsoft.Web/sites")) {
                 $res=$(az resource list --resource-type "$line" --query "length([?contains(id,'$Id_Comp')] && [?kind == 'app'])")
@@ -34,30 +35,27 @@ foreach ($id in $GroupList) {
             }
             
             # loop to count total PaaS resources for all ID's
-            if ($res -gt 0) {
-                # Sub Count is for count of PaaS resources
-                $sub_count = $sub_count + $res
-                # PaaS services are counted at 1:3 SU's - This script rounds the SU up to the next integer.
-                $su_count = $su_count + [Math]::Round($res/3, 0)
-                $total_count = $total_count + [Math]::Round($res/3, 0)
-            }
+            $sub_count = $sub_count + $res
+            
         }
+        $su_count = [Math]::Ceiling($sub_count/3)
+        $total_count = $total_count + $su_count
         echo "Subtotal for Platform as service $sub_count, SUs $su_count"
-
+ 
         $sub_count = 0
         $su_count = 0
         foreach ($line in Get-Content .\resources.faas.azure.list) {
             # echo "Line_ID $line for Id_Comp $Id_Comp"
             $res=$(az resource list --resource-type "$line" --query "length([?contains(id,'$Id_Comp')] && [?contains(kind,'functionapp')])")
-            if ($res -gt 0) {
-                $sub_count = $sub_count + $res
-                # FaaS services are counted at 1:20 SU's - This script rounds the SU up to the next integer.
-                $su_count = $su_count + [Math]::Round($res/20, 0)
-                $total_count = $total_count + [Math]::Round($res/20, 0)
-            }
+ 
+            $sub_count = $sub_count + $res
         }
+ 
+        # FaaS services are counted at 1:20 SU's - This script rounds the SU up to the next integer.
+        $su_count = [Math]::Ceiling($sub_count/20)
+        $total_count = $total_count + $su_count
         echo "Subtotal for Function as service $sub_count, SUs $su_count"
     }
 }
-
+ 
 echo "Total number of resources in all subscriptions: $total_count"
